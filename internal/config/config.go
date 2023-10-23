a4f4583aecd092b6484e7cc7b46e127c00ea66a8
kozyrev-m <mikhail1000@mail.ru>
2023-10-23 13:27:00 +0500

    inc4-store: add dsn to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,20 +7,23 @@ import (
 )
 
 var (
-	flagRunAddr  string
-	flagLogLevel string
+	flagRunAddr     string
+	flagLogLevel    string
+	flagDatabaseDSN string
 )
 
 // Config contains app configuration.
 type Config struct {
-	Address  string
-	LogLevel string
+	Address     string
+	LogLevel    string
+	DatabaseDSN string
 }
 
 // InitConfig sets app config.
 func InitConfig() *Config {
 	flag.StringVar(&flagRunAddr, "a", defaultRunAddr, "address and port to run server")
 	flag.StringVar(&flagLogLevel, "l", defaultLogLevel, "log level")
+	flag.StringVar(&flagDatabaseDSN, "d", "", "db address")
 	flag.Parse()
 
 	if envRunAddr := os.Getenv("RUN_ADDR"); envRunAddr != "" {
@@ -29,10 +32,14 @@ func InitConfig() *Config {
 	if envLogLevel := os.Getenv("LOG_LEVEL"); envLogLevel != "" {
 		flagLogLevel = envLogLevel
 	}
+	if envDatabaseDSN := os.Getenv("DATABASE_DSN"); envDatabaseDSN != "" {
+		flagDatabaseDSN = envDatabaseDSN
+	}
 
 	config := &Config{
-		Address:  flagRunAddr,
-		LogLevel: flagLogLevel,
+		Address:     flagRunAddr,
+		LogLevel:    flagLogLevel,
+		DatabaseDSN: flagDatabaseDSN,
 	}
 
 	return config
